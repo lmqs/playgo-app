a806790dc5cf317fc36719098f189b4930c6736f
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-04-07 22:19:59 -0300

    feat: ensure RemoteAuthentication returns an Account on sucess

--- tests/data/domain/mock-authentication.ts
+++
@@ -1,6 +0,0 @@
-import { AuthenticationParams } from "@/data/domain/usecases/authentication";
-
-export const mockAuthentication = () : AuthenticationParams => ({
-  user: 'any_user',
-  password: 'any_password'
-})
